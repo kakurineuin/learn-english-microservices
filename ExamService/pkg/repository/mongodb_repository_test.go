5ccdbbbdf2089873416795a0fd45adb1dc572a4f
Kevin <kakurineuin@gmail.com>
2023-12-22 08:25:47 +0800

    調整 repository 的測試

--- ExamService/pkg/repository/mongodb_repository_test.go
+++ ExamService/pkg/repository/mongodb_repository_test.go
@@ -5,21 +5,27 @@ import (
 	"fmt"
 	"log"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/suite"
 	"github.com/testcontainers/testcontainers-go"
 	"github.com/testcontainers/testcontainers-go/modules/mongodb"
 	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 
 	"github.com/kakurineuin/learn-english-microservices/exam-service/pkg/model"
 )
 
 type MyTestSuite struct {
 	suite.Suite
-	repo             DatabaseRepository
-	uri              string
-	ctx              context.Context
-	mongodbContainer *mongodb.MongoDBContainer
+	repo                    DatabaseRepository
+	uri                     string
+	ctx                     context.Context
+	mongodbContainer        *mongodb.MongoDBContainer
+	examIdForTestGetExam    string
+	examIdForTestUpdateExam string
+	examIdForTestDeleteExam string
 }
 
 func TestMyTestSuite(t *testing.T) {
@@ -42,6 +48,12 @@ func (s *MyTestSuite) SetupSuite() {
 		panic(err)
 	}
 
+	// 建立測試資料
+	err = createTestData(s, uri)
+	if err != nil {
+		panic(err)
+	}
+
 	s.repo = NewMongoDBRepository()
 	s.repo.ConnectDB(uri)
 	s.uri = uri
@@ -52,6 +64,11 @@ func (s *MyTestSuite) SetupSuite() {
 // run once, after test suite methods
 func (s *MyTestSuite) TearDownSuite() {
 	log.Println("TearDownSuite()")
+
+	// 不呼叫 panic，為了繼續往下執行去關閉 container
+	if err := s.repo.DisconnectDB(); err != nil {
+		log.Printf("DisconnectDB error: %v", err)
+	}
 
 	// Terminate container
 	if err := s.mongodbContainer.Terminate(s.ctx); err != nil {
@@ -97,73 +114,38 @@ func (s *MyTestSuite) TestCreateExam() {
 		Description: "desc01",
 		IsPublic:    true,
 		Tags:        []string{},
-		UserId:      "ddr01",
+		UserId:      "user01",
 	})
 	s.Nil(err)
 	s.NotEmpty(examId)
 }
 
 func (s *MyTestSuite) TestUpdateExam() {
+	id, err := primitive.ObjectIDFromHex(s.examIdForTestUpdateExam)
+	s.Nil(err)
+
 	ctx := context.TODO()
-
-	examId, err := s.repo.CreateExam(ctx, model.Exam{
-		Topic:       "TestUpdateExam",
-		Description: "desc01",
-		IsPublic:    true,
-		Tags:        []string{},
-		UserId:      "ddr01",
-	})
-	s.Nil(err)
-	s.NotEmpty(examId)
-
-	id, err := primitive.ObjectIDFromHex(examId)
-	s.Nil(err)
-
 	err = s.repo.UpdateExam(ctx, model.Exam{
 		Id:          id,
 		Topic:       "TestUpdateExam_u01",
 		Description: "desc01_u01",
 		IsPublic:    false,
-		Tags:        []string{"tag01", "tag02"},
-		UserId:      "ddr01",
+		Tags:        []string{"aaa", "bbb", "ccc"},
+		UserId:      "user01",
 	})
 	s.Nil(err)
 }
 
 func (s *MyTestSuite) TestGetExam() {
 	ctx := context.TODO()
-
-	examId, err := s.repo.CreateExam(ctx, model.Exam{
-		Topic:       "TestGetExam",
-		Description: "desc01",
-		IsPublic:    true,
-		Tags:        []string{},
-		UserId:      "ddr01",
-	})
-	s.Nil(err)
-	s.NotEmpty(examId)
-
-	exam, err := s.repo.GetExam(ctx, examId)
+	exam, err := s.repo.GetExam(ctx, s.examIdForTestGetExam)
 	s.Nil(err)
 	s.NotNil(exam)
 }
 
 func (s *MyTestSuite) TestFindExamsOrderByUpdateAtDesc() {
 	ctx := context.TODO()
-
-	for i := 0; i < 20; i++ {
-		examId, err := s.repo.CreateExam(ctx, model.Exam{
-			Topic:       fmt.Sprintf("TestFindExamsOrderByUpdateAtDesc_%d", i),
-			Description: "desc01",
-			IsPublic:    true,
-			Tags:        []string{},
-			UserId:      "ddr01",
-		})
-		s.Nil(err)
-		s.NotEmpty(examId)
-	}
-
-	exams, err := s.repo.FindExamsOrderByUpdateAtDesc(ctx, "ddr01", 10, 10)
+	exams, err := s.repo.FindExamsOrderByUpdateAtDesc(ctx, "user01", 10, 10)
 	s.Nil(err)
 	s.NotEmpty(exams)
 	s.Equal(10, len(exams))
@@ -171,17 +153,79 @@ func (s *MyTestSuite) TestFindExamsOrderByUpdateAtDesc() {
 
 func (s *MyTestSuite) TestDeleteExam() {
 	ctx := context.TODO()
+	err := s.repo.DeleteExam(ctx, s.examIdForTestDeleteExam)
+	s.Nil(err)
+}
 
-	examId, err := s.repo.CreateExam(ctx, model.Exam{
-		Topic:       "TestDeleteExam",
-		Description: "desc01",
+func createTestData(s *MyTestSuite, uri string) error {
+	ctx := context.TODO()
+	client, err := mongo.Connect(
+		ctx,
+		options.Client().ApplyURI(uri).SetTimeout(10*time.Second),
+	)
+	if err != nil {
+		return err
+	}
+
+	exams := []interface{}{}
+
+	for i := 0; i < 30; i++ {
+		exams = append(exams, model.Exam{
+			Topic:       fmt.Sprintf("topic_%d", i),
+			Description: "jsut for test",
+			Tags:        []string{"tag01", "tag02"},
+			IsPublic:    true,
+			UserId:      "user01",
+		})
+	}
+	collection := client.Database("learnEnglish").Collection("exams")
+	_, err = collection.InsertMany(ctx, exams)
+	if err != nil {
+		return err
+	}
+
+	// For TestGetExam
+	result, err := collection.InsertOne(ctx, model.Exam{
+		Topic:       "TestGetExam",
+		Description: "jsut for test",
+		Tags:        []string{"tag01", "tag02"},
 		IsPublic:    true,
-		Tags:        []string{},
-		UserId:      "ddr01",
+		UserId:      "user01",
 	})
-	s.Nil(err)
-	s.NotEmpty(examId)
+	if err != nil {
+		return err
+	}
+	s.examIdForTestGetExam = result.InsertedID.(primitive.ObjectID).Hex()
 
-	err = s.repo.DeleteExam(ctx, examId)
-	s.Nil(err)
+	// For TestUpdateExam
+	result, err = collection.InsertOne(ctx, model.Exam{
+		Topic:       "TestUpdateExam",
+		Description: "jsut for test",
+		Tags:        []string{"tag01", "tag02"},
+		IsPublic:    true,
+		UserId:      "user01",
+	})
+	if err != nil {
+		return err
+	}
+	s.examIdForTestUpdateExam = result.InsertedID.(primitive.ObjectID).Hex()
+
+	// For TestDeleteExam
+	result, err = collection.InsertOne(ctx, model.Exam{
+		Topic:       "TestDeleteExam",
+		Description: "jsut for test",
+		Tags:        []string{"tag01", "tag02"},
+		IsPublic:    true,
+		UserId:      "user01",
+	})
+	if err != nil {
+		return err
+	}
+	s.examIdForTestDeleteExam = result.InsertedID.(primitive.ObjectID).Hex()
+
+	if err := client.Disconnect(ctx); err != nil {
+		return err
+	}
+
+	return nil
 }
